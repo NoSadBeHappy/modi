7093c5a57cda6ac0ec271f78e911f281a0ffa4d4
Trevor <trevor271c@protonmail.com>
2025-04-11 11:25:15 -0700

    more stuff

--- modi.py
+++ modi.py
@@ -1,22 +1,17 @@
 import py_midicsv as pm
 
+#midi info: https://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
+
 # Load the MIDI file and parse it into CSV format
 csv_string_list = pm.midi_to_csv("example.mid")
 
 with open("example_converted.csv", "w") as f:
     f.writelines(csv_string_list)
 
-#subtract the key signature number from all note numbers in colum E to get to C. (record this value in a var)
-
-
-
-
-
-
-
-
-
-
+#subtract the key signature number from all note numbers in colum e get to C. (record this value in a var)
+#convert to major if in minor by +1 from all the E, A, and B numbers.
+#convert to desired mode
+#add key signature to get back to the right key
 
 
 
