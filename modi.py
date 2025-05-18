2859bbee1cfb1cddc4f1dbc49f24f21faadd15ed
Trevor <trevor271c@protonmail.com>
2025-05-18 09:35:42 -0700

    bunch o stuff

--- modi.py
+++ modi.py
@@ -1,4 +1,5 @@
 import py_midicsv as pm
+import csv
 
 A = [9, 21, 33, 45, 57, 69, 81, 93, 105, 117]
 B = [11, 23, 35, 47, 59, 71, 83, 95, 107, 119]
@@ -20,50 +21,61 @@ locrian = [B, E, A, D, G]
 # Load the MIDI file and parse it into CSV format
 csv_midi = pm.midi_to_csv("example.mid")
 
+#to csv
 with open("example_converted.csv", "w") as f:
     f.writelines(csv_midi)
 
+#to list
+list_csv_midi = list(csv.reader(csv_midi, delimiter=","))
+
+
 #subtract the key signature number from all note numbers in colum e get to C. (record this value in a var)
 
-KeySignature = csv_midi.index("Key_signature") + 1
+KeySignature = int(list_csv_midi[list_csv_midi.index( ' Key_signature') + 1])
 
-pos = 0
+pos = int(0)
 
-mode = input("what mode do you want it in? (put the tonic in Major, so 1: ionian (Major), 2: Dorian, 3: Phrygian. 4: Lydian, 5: Mixolydian, 6: Aeolian (minor), 7: Locrian") #key change amount
-minor = input("is the input midi file in minor?")#true or false is current in minor or major
+mode = input("what mode do you want it in? (put the tonic in Major, so 1: ionian (Major), 2: Dorian, 3: Phrygian. 4: Lydian, 5: Mixolydian, 6: Aeolian (minor), 7: Locrian: ") #key change amount
+minor = input("is the input midi file in minor? (true or false): ")#true or false is current in minor or major
 
-pos = index("Notes_on_c", pos) #skip to the first one
-for pos in csv_midi: #always adds 1 to pos every time it loops, this looks for Note_on_c and WILL LOOK FOR Note_off_c
-    if (pos == "Note_on_c"):
-        #pos is now the position where the int we need to change is
-        pos = pos + 2
-    csv_midi[pos] = csv_midi[pos] - KeySignature #go to c temperarly to more easily change mode
-    if (minor == true): #convert to major if needed
-        if csv_midi[pos] in aeolian:
-            #convert minor to major
-            csv_midi[pos] = csv_midi + 1
+print (pos)
+for line in list_csv_midi:
+    for pos in line: #always adds 1 to pos every time it loops, this looks for Note_on_c and WILL LOOK FOR Note_off_c
+        print (pos)
+        if (list_csv_midi[line][int(float(pos))] == " Note_on_c"):
+            #pos is now the position where the int we need to change is
+            pos = pos + 2
+            list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) - KeySignature #go to c temperarly to more easily change mode
+        if (minor == True): #convert to major if needed
+            if int(list_csv_midi[line][int(float(pos))]) in aeolian:
+                #convert minor to major
+                list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) + 1
 
-    #mode == 1 is not needed, its already in major so yay!
-    if (mode == 2): # lower B E
-        if csv_midi[pos] in dorian:
-            csv_midi[pos] = csv_midi[pos] - 1
-    elif (mode == 3): # lower B E A D
-        if csv_midi[pos] in phrygian:
-            csv_midi[pos] = csv_midi[pos] - 1
-    elif (mode == 4): #raise F
-        if csv_midi[pos] in lydian:
-            csv_midi[pos] = csv_midi[pos] + 1
-    elif (mode == 5): #lower B
-        if csv_midi[pos] in mixolydian:
-            csv_midi[pos] = csv_midi[pos] - 1
-    elif (mode == 6): #lower B E A
-        if csv_midi[pos] in aeolian:
-            csv_midi[pos] = csv_midi[pos] - 1
-    elif (mode == 7): #lower B E A D G
-        if csv_midi[pos] in locrian:
-            csv_midi[pos] = csv_midi[pos] - 1
+        #mode == 1 is not needed, its already in major so yay!
+        if (mode == 2): # lower B E
+            if int(list_csv_midi[line][int(float(pos))]) in dorian:
+                list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) - 1
+        elif (mode == 3): # lower B E A D
+            if int(list_csv_midi[line][int(float(pos))]) in phrygian:
+                list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) - 1
+        elif (mode == 4): #raise F
+            if int(list_csv_midi[line][int(float(pos))]) in lydian:
+                list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) + 1
+        elif (mode == 5): #lower B
+            if int(list_csv_midi[line][int(float(pos))]) in mixolydian:
+                list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) - 1
+        elif (mode == 6): #lower B E A
+            if int(list_csv_midi[line][int(float(pos))]) in aeolian:
+                list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) - 1
+        elif (mode == 7): #lower B E A D G
+            if int(list_csv_midi[line][int(float(pos))]) in locrian:
+                list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) - 1
+        list_csv_midi[line][int(float(pos))] = int(list_csv_midi[line][int(float(pos))]) + KeySignature #get back to real key at the end
 
-    csv_midi[pos] = csv_midi[pos] + KeySignature #get back to real key at the end
+
+#ADD A LIST TO CSV PARSER
+
+
 
 # Parse the CSV output of the previous command back into a MIDI file
 midi_object = pm.csv_to_midi(csv_string_list)
